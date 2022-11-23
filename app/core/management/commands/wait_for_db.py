bff0c9b05e34a4b86841edb112c801cbe61d0234
nallag <gangireddy.nallagond@dell.com>
2022-11-23 21:28:26 +0530

    new changes in code

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,8 @@
+    """Django commands to wait for the database to be available
+    """
+    from django.core.management.base import BaseCommand
+    
+    class Command(BaseCommand)
+
+        def handle(self, *args, **options):
+            pass
