ffa5ecfeb32dbdcb49e8a19d3e4dffa4ec2bc438
nallag <gangireddy.nallagond@dell.com>
2022-11-23 17:55:17 +0530

    new DB and postgresql env

---
+++ app/app/tests.py
@@ -0,0 +1,11 @@
+"""Smaple tests"""
+
+from django.test import SimpleTestCase
+from app import calc    
+
+class Calctests(SimpleTestCase):
+    
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+        
