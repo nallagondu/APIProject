12bdebf15a4e2be29fa02df885e723994b386a18
nallag <gangireddy.nallagond@dell.com>
2022-11-29 23:46:05 +0530

    73. review userAPI in browser

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,32 +1,49 @@
-'''Serializers for the user API view '''
+"""
+Serializers for the user API View.
+"""
 from django.contrib.auth import (
     get_user_model,
     authenticate,
 )
 from django.utils.translation import gettext as _
+
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
-    '''Serializer the user objects'''
+    """Serializer for the user object."""
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
-        '''Create and return a user with encrypted password'''
+        """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
-class AuthTokenSerilizer(serializers.Serializer):
-    '''serelizer for the user auth token'''
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
     email = serializers.EmailField()
-    password  = serializers.CharField(
-        style= {'input_type': 'password'},
+    password = serializers.CharField(
+        style={'input_type': 'password'},
         trim_whitespace=False,
     )
 
     def validate(self, attrs):
-        '''validate and athenticate the user'''
+        """Validate and authenticate the user."""
         email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
@@ -35,7 +52,8 @@ class AuthTokenSerilizer(serializers.Serializer):
             password=password,
         )
         if not user:
-            msg= _('Unable to authenticate using provided credentails')
-            raise serializers.ValidationError(msg, code='authentication')
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
         attrs['user'] = user
         return attrs
